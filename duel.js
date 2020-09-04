327285d640bb34e8025369c25fb26a884b78cb0c
Mark Enders <endersma@gmail.com>
2020-09-04 15:06:38 -0700

    finished assignment

--- duel.js
+++ duel.js
@@ -3,6 +3,7 @@ class Card {
         this.name = name;
         this.cost = cost;
     }
+
 }
 
 class Unit extends Card {
@@ -11,7 +12,10 @@ class Unit extends Card {
         this.power = power;
         this.res = res;
     }
-
+    attack(target) {
+        target.res -= this.power;
+        console.log(`${this.name} attacked ${target.name} for ${this.power} damage!`);
+    }
 }
 
 class Effect extends Card {
@@ -21,10 +25,26 @@ class Effect extends Card {
         this.stat = stat;
         this.mag = mag;
     }
-}
+    play(target) {
+        if( target instanceof Unit ) {
+            target[this.stat] += this.mag;
+            console.log(`${this.name} was played on ${target.name}`);
+            console.log(this.desc);
+        } 
+        else {
+            throw new Error( "Target must be a unit!" );
+            }
+        }
+    }
+
 
 const rbninja = new Unit("Red Belt Ninja", 3, 3, 4);
-const bbninja = new Unit("Black Belt Ninja", 4, 5, 4);
 const hardalgo = new Effect("Hard Algorithm", 2, "Increase target's resilience by 3", "resilience", 3);
+hardalgo.play(rbninja);
+const bbninja = new Unit("Black Belt Ninja", 4, 5, 4);
 const uprejection = new Effect("Unhandled Promise Rejection", 1, "Reduce target's resilience by 2", "resilience", 2);
-const pairprog = new Effect("Pair Programming", 3, "increase target's power by 2", "power", 2);
+const pairprog = new Effect("Pair Programming", 3, 
+"increase target's power by 2", "power", 2);
+uprejection.play(rbninja);
+pairprog.play(rbninja);
+rbninja.attack(bbninja)
